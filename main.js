cd864af03d9b9647eb1b060a6327066c6be67114
glenn <glenn.ludszuweit@gmail.com>
2020-03-26 15:27:50 +0100

    register individual routes

--- main.js
+++ main.js
@@ -4,3 +4,56 @@ const httpStatus = require("http-status-codes");
 const router = require("./router");
 const contentTypes = require("./contentTypes");
 const utils = require("./utils");
+
+router.get("/", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/index.html", res);
+});
+
+router.get("/courses", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/courses.html", res);
+});
+
+router.get("/contact", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/contact.html", res);
+});
+
+router.post("/courses", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/courses.html", res);
+});
+
+router.get("/graph.png", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.png);
+  utils.getFile("public/images/graph.png", res);
+});
+
+router.get("/people.jpg", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.jpg);
+  utils.getFile("public/images/people.jpg", res);
+});
+
+router.get("/product.jpg", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.jpg);
+  utils.getFile("public/images/product.jpg", res);
+});
+
+router.get("/kitchen_hub.css", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.css);
+  utils.getFile("public/css/kitchen_hub.css", res);
+});
+
+router.get("/bootstrap.css", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.css);
+  utils.getFile("public/css/bootstrap.css", res);
+});
+
+router.get("/kitchenHub.js", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.js);
+  utils.getFile("public/js/kitchenHub.js", res);
+});
+
+http.createServer(router.handle).listen(port);
+console.log(`Server running on PORT: ${port}`);
