0d136533b4263d08c2de749e89b2e603cc6278a1
glenn <glenn.ludszuweit@gmail.com>
2020-03-28 19:48:47 +0100

    pass and loop content to views

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,8 +2,24 @@ exports.indexPage = (req, res) => {
   res.render("index");
 };
 
+var courses = [
+  {
+    title: "Event Driven Cakes.",
+    cost: 50
+  },
+  {
+    title: "Asychronous Artichokes",
+    cost: 50
+  },
+  {
+    title: "Object Oriented Orange Juice",
+    cost: 50
+  }
+];
 exports.showCourses = (req, res) => {
-  res.render("courses");
+  res.render("courses", {
+    offeredCourses: courses
+  });
 };
 
 exports.showSignUp = (req, res) => {
