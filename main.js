601fdc847b0b337bbeb0852da1c15e7cf73f0cfc
glenn <glenn.ludszuweit@gmail.com>
2020-03-28 17:48:10 +0100

    added body parsing and layout in main.js

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const httpStatus = require("http-status-codes");
+const layouts = require("express-ejs-layouts");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+
 //MIDDLEWARE
+app.use(layouts);
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
 
 //ROUTE REGISTER
 app.get("/", (req, res) => {
-  res.send(`<h1>Welcome!</h1>`);
+  res.render("index");
 });
 
 //SERVER
