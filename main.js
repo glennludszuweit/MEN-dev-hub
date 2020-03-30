1f0c21cb28f80f635fb1fc7c57e1a9eb85eac393
glenn <glenn.ludszuweit@gmail.com>
2020-03-30 13:37:30 +0200

    Setup Mongoose

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const httpStatus = require("http-status-codes");
 const layouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 const homeController = require("./controllers/homeController");
 const errorController = require("./controllers/errorController");
@@ -11,19 +12,10 @@ const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 
 //DATABASE
-const mongoDB = require("mongodb").MongoClient;
-const dbURL = "mongodb://localhost:27017";
-const dbName = "kitchenhub";
-
-mongoDB.connect(dbURL, (error, client) => {
-  if (error) throw error;
-  let db = client.db(dbName);
-  db.collection("contacts")
-    .find()
-    .toArray((error, data) => {
-      if (error) throw error;
-      console.log(data);
-    });
+mongoose.connect("mongodb://localhost:27017", { useNewUrlParser: true });
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("MongoDB connection made!");
 });
 
 //MIDDLEWARE
