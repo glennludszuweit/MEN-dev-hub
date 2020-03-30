7b37ec50d0b81ff330953a2c6f4a6e4d284b9aa3
glenn <glenn.ludszuweit@gmail.com>
2020-03-30 14:38:32 +0200

    moved schema and model to separate module

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const homeController = require("./controllers/homeController");
 const errorController = require("./controllers/errorController");
 
+const Subscriber = require("./models/subscriber");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -29,34 +31,33 @@ db.once("open", () => {
 });
 
 //SCHEMA
-const subsSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  zipCode: Number
+var myQuery = Subscriber.findOne({
+  name: "Tony Patterdale"
+}).where("email", /patterdale/);
+myQuery.exec((error, data) => {
+  if (data) console.log(data.name);
 });
 
-const Subscriber = mongoose.model("Subscriber", subsSchema);
+// var subs1 = new Subscriber({
+//   name: "Tony Patterdale",
+//   email: "t.patterdale@gmail.com"
+// });
 
-var subs1 = new Subscriber({
-  name: "Tony Patterdale",
-  email: "t.patterdale@gmail.com"
-});
+// subs1.save((error, savedDocument) => {
+//   if (error) console.log(error);
+//   console.log(savedDocument);
+// });
 
-subs1.save((error, savedDocument) => {
-  if (error) console.log(error);
-  console.log(savedDocument);
-});
-
-Subscriber.create(
-  {
-    name: "Tony Patterdale",
-    email: "t.patterdale@gmail.com"
-  },
-  function(error, savedDocument) {
-    if (error) console.log(error);
-    console.log(savedDocument);
-  }
-);
+// Subscriber.create(
+//   {
+//     name: "Tony Patterdale",
+//     email: "t.patterdale@gmail.com"
+//   },
+//   function(error, savedDocument) {
+//     if (error) console.log(error);
+//     console.log(savedDocument);
+//   }
+// );
 
 //ROUTE REGISTER
 app.get("/", homeController.indexPage);
