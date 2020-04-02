f1878bcd219b3ff5070de7cab6abf0ac7b64ab42
glenn <glenn.ludszuweit@gmail.com>
2020-04-02 10:40:39 +0200

    added validation to subscriber schema

--- models/subscriber.js
+++ models/subscriber.js
@@ -1,9 +1,29 @@
 const mongoose = require("mongoose");
 
 const subscriberSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  telNumber: Number
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true
+  },
+  telNumber: {
+    type: Number
+  }
 });
+
+subscriberSchema.methods.getInfo = function() {
+  return `Name: ${this.name} Email: ${this.email} Tel Number: ${this.telNumber}`;
+};
+
+subscriberSchema.methods.findLocalSubscribers = function() {
+  return this.model("Subscriber")
+    .find({ name: this.name })
+    .exec();
+};
 
 module.exports = mongoose.model("Subscriber", subscriberSchema);
