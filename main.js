5dc923edfd545dec9b88f782b6f9a1e653c271fa
glenn <glenn.ludszuweit@gmail.com>
2020-04-26 13:05:11 +0200

    attached/required socket.io to server

--- main.js
+++ main.js
@@ -86,6 +86,7 @@ app.post("/send", (req, res) => {
 });
 
 //Server
-app.listen(app.get("port"), () => {
+const server = app.listen(app.get("port"), () => {
   console.log(`Server running at http://localhost:${app.get("port")}`);
 });
+const io = require("socket.io")(server);
