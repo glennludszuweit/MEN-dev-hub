5af447568fd6bf810b8eade5f7afadb157bd879e
glenn <glenn.ludszuweit@gmail.com>
2020-04-27 16:18:01 +0200

    saving chats to database

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -8,11 +8,18 @@ module.exports = (io) => {
     });
     client.on("message", (data) => {
       let messageAttributes = {
-        content: data.content,
-        userName: data.userName,
-        userId: data.userId,
-      };
-      io.emit("message", messageAttributes);
+          content: data.content,
+          userName: data.userName,
+          userId: data.userId,
+        },
+        m = new Chat(messageAttributes);
+      m.save()
+        .then(() => {
+          io.emit("message", messageAttributes);
+        })
+        .catch((error) => {
+          console.log(`error: ${error.message}`);
+        });
     });
   });
 };
