62068d07e6b2cbefa110dc4dd6d3c8c8ff7ef0e6
glenn <glenn.ludszuweit@gmail.com>
2020-04-27 12:37:41 +0200

    display all users in chat page

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const homeController = require("../controllers/homeController");
+const usersController = require("../controllers/usersController");
 
 router.get("/", homeController.index);
-router.get("/chat", homeController.chat);
+router.get("/chat", usersController.index, homeController.chat);
 
 module.exports = router;
