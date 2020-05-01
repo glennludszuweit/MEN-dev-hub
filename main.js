973dabab6fff1da9923b23403fa257e8842677ba
glenn <glenn.ludszuweit@gmail.com>
2020-05-01 20:13:13 +0200

    heroku fix 9

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/kitchenhub",
+  "mongodb+srv://devhub:admin123@node-devhub-mwfvv.mongodb.net/test?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
   }
